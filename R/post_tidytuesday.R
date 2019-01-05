5d91b8cf1ead1e08ca20abbbe187d94224429a26
Thomas Mock <j.thomasmock@gmail.com>
2019-01-04 20:45:52 -0600

    add post_tidytuesday function

---
+++ R/post_tidytuesday.R
@@ -0,0 +1,42 @@
+#' Create the R4DS Community TidyTuesday posts
+#'
+#' @import rtweet glue emo lubridate here tinycc purrr
+#' @export
+#'
+
+post_tidytuesday <- function(exploring, long_link){
+
+    # Authenticate each session
+    tinycc::auth(secret_login, secret_api_key)
+
+    # set date for files structure and names
+    week_date <- as.character(lubridate::today() + 1)
+    week_num <- as.numeric((lubridate::today() + 1) - lubridate::ymd(20190101))/7
+
+
+    # Generate the short_link
+    short_link <- tinycc::shorten(longURL = long_link,
+                                  shortURL = glue::glue("tt_2019_{week_num}")) %>%
+        purrr::pluck("results", "short_url")
+
+    # post the tweet with fill
+    rtweet::post_tweet(status = glue::glue(
+        "The @R4DScommunity welcomes you to week {week_num} of #TidyTuesday!  We're exploring {exploring}!
+
+",
+        emo::ji("folder"),
+        " http://bit.ly/tidyreadme
+",
+        emo::ji("news"),
+        " {short_link}
+
+#r4ds #tidyverse #rstats #dataviz"),
+
+        # The below code is relative to my project
+        # You need to specify path to the images for tweeting
+        media = c(here::here("static_img", "tt_logo.png"),
+                  here::here("static_img", "tt_rules.png"),
+                  here::here("2019", week_date, "pic1.png"),
+                  here::here("2019", week_date, "pic1.png")
+        ))
+}
