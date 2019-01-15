0017f4304a51fcc5bc92a67f975f9eb3bf1bcdb4
Thomas Mock <j.thomasmock@gmail.com>
2019-01-15 05:54:02 -0600

    fix post fxn

--- R/post_tidytuesday.R
+++ R/post_tidytuesday.R
@@ -4,21 +4,15 @@
 #' @export
 #'
 
-post_tidytuesday <- function(exploring, long_link){
 
-    # Authenticate each session
-    tinycc::auth(tinycc_login = Sys.getenv("secret_login"),
-                 tinycc_api_key = Sys.getenv("secret_api_key"))
+post_tidytuesday <- function(exploring, short_link){
 
     # set date for files structure and names
     week_date <- as.character(lubridate::today() + 1)
-    week_num <- as.numeric((lubridate::today() + 1) - lubridate::ymd(20190101))/7
 
-
-    # Generate the short_link
-    short_link <- tinycc::shorten(longURL = long_link,
-                                  shortURL = glue::glue("tt_2019_{week_num}")) %>%
-        purrr::pluck("results", "short_url")
+    # Today's date + 1 = tomorrow
+    # Then time diff between tomorrow and 1st tidytuesday in number of weeks
+    week_num <- as.numeric((lubridate::today() + 1) - lubridate::ymd(20190101))/7 + 1
 
     # post the tweet with fill
     rtweet::post_tweet(status = glue::glue(
@@ -38,6 +32,6 @@ post_tidytuesday <- function(exploring, long_link){
         media = c(here::here("static_img", "tt_logo.png"),
                   here::here("static_img", "tt_rules.png"),
                   here::here("2019", week_date, "pic1.png"),
-                  here::here("2019", week_date, "pic1.png")
+                  here::here("2019", week_date, "pic2.png")
         ))
 }
