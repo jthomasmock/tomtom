069a6d417d3101bda2b17dc7f1cbfe7d794e21e2
Thomas Mock <j.thomasmock@gmail.com>
2019-07-26 15:52:35 -0500

    move scale functions and update palette_picker to take reverse argument

---
+++ R/scale_functions.R
@@ -0,0 +1,52 @@
+#' @export
+tomtom_pal <- function(palette = "main", reverse = FALSE, ...) {
+    pal <- tomtom_palettes[[palette]]
+
+    if (reverse) pal <- rev(pal)
+
+    colorRampPalette(pal, ...)
+}
+
+#' Color scale constructor for tomtom colors
+#'
+#' @param palette Character name of palette in tomtom_palettes
+#' @param discrete Boolean indicating whether color aesthetic is discrete or not
+#' @param reverse Boolean indicating whether the palette should be reversed
+#' @param ... Additional arguments passed to discrete_scale() or
+#'            scale_color_gradientn(), used respectively when discrete is TRUE or FALSE
+#' @export
+#'
+
+
+scale_color_tomtom <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+    pal <- tomtom_pal(palette = palette, reverse = reverse)
+
+    if (discrete) {
+        discrete_scale("colour", paste0("tomtom_", palette), palette = pal, ...)
+    } else {
+        scale_color_gradientn(colours = pal(256), ...)
+    }
+}
+
+#scale_color_tomtom <- scale_colour_tomtom
+
+#' Fill scale constructor for tomtom colors
+#'
+#' @param palette Character name of palette in tomtom_palettes
+#' @param discrete Boolean indicating whether color aesthetic is discrete or not
+#' @param reverse Boolean indicating whether the palette should be reversed
+#' @param ... Additional arguments passed to discrete_scale() or
+#'            scale_color_gradientn(), used respectively when discrete is TRUE or FALSE
+#'
+#' @export
+
+scale_fill_tomtom <- function(palette = "main", discrete = TRUE,
+                              reverse = FALSE, ...) {
+    pal <- tomtom_pal(palette = palette, reverse = reverse)
+
+    if (discrete) {
+        discrete_scale("colour", paste0("tomtom_", palette), palette = pal, ...)
+    } else {
+        scale_color_gradientn(colours = pal(256), ...)
+    }
+}
--- R/theme_tom.R
+++ R/theme_tom.R
@@ -95,52 +95,6 @@ tomtom_cols <- function(...) {
     tomtom_colors[cols]
 }
 
-#' Color scale constructor for tomtom colors
-#'
-#' @param palette Character name of palette in tomtom_palettes
-#' @param discrete Boolean indicating whether color aesthetic is discrete or not
-#' @param reverse Boolean indicating whether the palette should be reversed
-#' @param ... Additional arguments passed to discrete_scale() or
-#'            scale_color_gradientn(), used respectively when discrete is TRUE or FALSE
-#' @export
-#'
-
-
-scale_color_tomtom <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
-    pal <- tomtom_pal(palette = palette, reverse = reverse)
-
-    if (discrete) {
-        discrete_scale("colour", paste0("tomtom_", palette), palette = pal, ...)
-    } else {
-        scale_color_gradientn(colours = pal(256), ...)
-    }
-}
-
-#scale_color_tomtom <- scale_colour_tomtom
-
-#' Fill scale constructor for tomtom colors
-#'
-#' @param palette Character name of palette in tomtom_palettes
-#' @param discrete Boolean indicating whether color aesthetic is discrete or not
-#' @param reverse Boolean indicating whether the palette should be reversed
-#' @param ... Additional arguments passed to discrete_scale() or
-#'            scale_color_gradientn(), used respectively when discrete is TRUE or FALSE
-#'
-#' @export
-
-scale_fill_tomtom <- function(palette = "main", discrete = TRUE,
-                              reverse = FALSE, ...) {
-    pal <- tomtom_pal(palette = palette, reverse = reverse)
-
-    if (discrete) {
-        discrete_scale("colour", paste0("tomtom_", palette), palette = pal, ...)
-    } else {
-        scale_color_gradientn(colours = pal(256), ...)
-    }
-}
-
-
-
 #' @export
 tomtom_palettes <- list(
     `main`  = tomtom_cols("blue", "red", "orange"),
@@ -161,21 +115,18 @@ tomtom_palettes <- list(
 
 )
 
-#' @export
-tomtom_pal <- function(palette = "main", reverse = FALSE, ...) {
-    pal <- tomtom_palettes[[palette]]
 
-    if (reverse) pal <- rev(pal)
-
-    colorRampPalette(pal, ...)
-}
 
 #' @export
 #'
-tom_pal_picker <- function(name = "main", n, type = c("discrete", "continuous")) {
+palette_picker <- function(name = "main", n, type = c("discrete", "continuous"),
+                           reverse = FALSE) {
     type <- match.arg(type)
 
     pal <- tomtom_palettes[[name]]
+
+    if (reverse) pal <- rev(pal)
+
     if (is.null(pal))
         stop("Palette not found.")
 
